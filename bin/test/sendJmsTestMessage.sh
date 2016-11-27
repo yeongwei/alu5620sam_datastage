d7d12281bf5c2bf11dee24cc936ff2a48fb34f26
YeongWei <laiyw@persistent.com>
2016-11-27 20:01:08 +0530

    Add test script for JMS messages (development use)

---
+++ bin/test/sendJmsTestMessage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Licensed Materials - Property of IBM
+#     5724-W86, 5724-X63
+# Copyright IBM Corporation 2013. All Rights Reserved.
+# US Government Users Restricted Rights- Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# Wrapper script to replicate File available events
+#
+
+cd $(dirname $0)/..
+source ./env.sh
+
+EVENT_MESSAGE="$3"
+
+./sendJmsMessage.sh tcp://localhost:61616 $SAM_UNIQUE_ID "$EVENT_MESSAGE"
